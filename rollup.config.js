c270ce48c88a7afc182a6d5abdc7a89eaa581127
Pavel Galkin <pavel_galkin@epam.com>
2017-05-24 13:51:26 +0300

    Change Readme.md and add uglify plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import vue from 'rollup-plugin-vue';
 import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: './src/index.js',
   dest: 'vue-yandex-maps.js',
   plugins: [
     vue({compileTemplate: true}),
-    buble()
+    buble(),
+    uglify()
   ],
   format: 'umd',
   moduleName: 'vueYandexMaps'
