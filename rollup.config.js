d3f6402e0406926d086ff8bdbae5b93d78e1ef43
PNKBizz <bizz.gc@gmail.com>
2017-08-02 21:29:03 +0300

    Remove cdn version

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import vue from 'rollup-plugin-vue';
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: './src/index.js',
   dest: 'vue-yandex-maps.js',
   plugins: [
+    resolve(),
     vue({compileTemplate: true}),
     buble(),
     uglify()
