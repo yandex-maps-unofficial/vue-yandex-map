65798eb9781e3c4d3c0015915b5eed6bb1cdda9d
Pavel Galkin <bizz.gc@gmail.com>
2022-08-14 12:11:15 +0300

    actions fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
         title: 'Vue-yandex-maps',
         description: 'Я.Карты для Vue JS',
         nav: [
-          { text: 'Главная', link: '/' },
+          { text: 'v0.x', items: [{ text: 'v1.x-beta', link: 'https://vue-yandex-maps.github.io/new-docs/' }] },
           { text: 'Руководство', link: '/guide/' },
           { text: 'Примеры', link: '/examples/' },
           { text: 'API Я.Карт', link: 'https://tech.yandex.ru/maps/doc/jsapi/2.1/quick-start/index-docpage/' },
@@ -27,7 +27,7 @@ module.exports = {
         title: 'Vue-yandex-maps',
         description: 'Yandex map component for Vue JS',
         nav: [
-          { text: 'Main', link: '/en/' },
+          { text: 'v0.x', items: [{ text: 'v1.x-beta', link: 'https://vue-yandex-maps.github.io/new-docs/' }] },
           { text: 'Guide', link: '/en/guide/' },
           { text: 'Examples', link: '/en/examples/' },
           { text: 'YandexMap API ', link: 'https://tech.yandex.ru/maps/doc/jsapi/2.1/quick-start/index-docpage/' },
