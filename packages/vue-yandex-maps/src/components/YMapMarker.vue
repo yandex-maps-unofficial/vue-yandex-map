5b0830bcac691eefe7a1c35e6c76ea0ecc45099b
daniluk4000 <daniluk4000@outlook.com>
2023-07-30 18:58:23 +0300

    Add Marker

---
+++ packages/vue-yandex-maps/src/components/YMapMarker.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import {
+  onMounted, watch, h, PropType,
+  defineComponent,
+} from 'vue';
+import {
+  BehaviorEvents, DomEvents, MapEvents, YMapListener,
+} from '@yandex/ymaps3-types';
+import { injectMap, waitTillMapInit } from '../composables/utils';
+
+export default defineComponent({
+  name: 'YMapListener',
+  props: {
+    settings: {
+      type: Object as PropType<Partial<DomEvents & MapEvents & BehaviorEvents>>,
+      default: () => ({}),
+    },
+  },
+  setup(props, { slots }) {
+    const map = injectMap();
+
+    let mapListener: YMapListener | undefined;
+
+    watch(props, () => {
+      mapListener?.update(props.settings || {});
+    }, {
+      deep: true,
+    });
+
+    onMounted(async () => {
+      await waitTillMapInit();
+
+      mapListener = new ymaps3.YMapListener(props.settings || {});
+      map.value?.addChild(mapListener);
+    });
+
+    return () => h('div', slots.default?.());
+  },
+});
+</script>
